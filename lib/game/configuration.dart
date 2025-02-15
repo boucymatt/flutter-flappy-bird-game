da9daa8a835259a89af06fcdfd51e101c0e01461
boucymatt <boucymatt@pm.me>
2025-02-15 14:16:49 +0000

    game speed slightly reduced

--- lib/game/configuration.dart
+++ lib/game/configuration.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const gameSpeed = 200.0;
+  static const gameSpeed = 150.0;
   static const groundHeight = 110.0;
   static const pipeInterval = 1.3;
   static const birdVelocity = 260;
